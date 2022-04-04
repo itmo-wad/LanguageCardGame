f6912921fe46a890fe64eff9788cde7f45b9c1ed
danmerenkov <danmerenkov@gmail.com>
2022-04-04 10:54:19 +0300

    Add script for automatic data insert to database and some other fixes
    * Small fixes in templates (regarding downloading images)

--- database_setup.py
+++ database_setup.py
@@ -1,3 +1,31 @@
-from models import Base, engine
+import csv
+
+import sqlalchemy
+
+from models import Base, engine, session, Category, Word
 
 Base.metadata.create_all(engine)
+
+with open("static/Categories.csv", 'r', newline='', encoding='utf-8-sig') as f:
+    reader = csv.reader(f, delimiter=';')
+    categories = list(reader)
+
+for category in categories:
+    try:
+        record = Category(id=category[0], ru_name=category[1], en_name=category[2], image=category[3])
+        session.add(record)
+        session.commit()
+    except sqlalchemy.exc.IntegrityError:
+        session.rollback()
+
+with open("static/Words.csv", 'r', newline='', encoding='utf-8-sig') as f:
+    reader = csv.reader(f, delimiter=';')
+    words = list(reader)
+
+for word in words:
+    try:
+        record = Word(id=word[0], category_id=word[1], ru_name=word[2], en_name=word[3])
+        session.add(record)
+        session.commit()
+    except sqlalchemy.exc.IntegrityError:
+        session.rollback()
